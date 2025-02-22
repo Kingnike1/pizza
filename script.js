550d01a7d56a65b5f09b2abf351c4cabe4bae627
Kingnike1 <pablo.rodrigues1@estudante.ifgoiano.edu.br>
2025-02-22 15:52:46 -0300

    adicionando o firebase dentro do codigo

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDU0_pk_UzpaudRkHAUkBDuvysWuAE1YEQ",
+    authDomain: "pizza-88154.firebaseapp.com",
+    databaseURL: "https://pizza-88154-default-rtdb.firebaseio.com",
+    projectId: "pizza-88154",
+    storageBucket: "pizza-88154.firebasestorage.app",
+    messagingSenderId: "494260841198",
+    appId: "1:494260841198:web:8576c107cebef0e0f461ff"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig)
+const db = firebase.database()
 document.addEventListener("DOMContentLoaded", () => {
     if (typeof firebase === "undefined") {
         console.error("Firebase não foi carregado corretamente!");
